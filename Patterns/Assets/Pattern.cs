d92d752d49cbc99717ea72a9939c7a833e4c766c
Blaer <blaerzhang@gmail.com>
2024-04-01 16:37:02 -0400

    First Commit

---
+++ Patterns/Assets/Pattern.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Pattern : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+        // Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern; Pattern isPattern; Pattern isNotPattern;
+        // if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern; if (this is Pattern) Pattern pattern = this is Pattern ? isPattern : isNotPattern;
+
+    }
+
+
+}
